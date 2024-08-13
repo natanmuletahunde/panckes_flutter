9e0f7e8eeaec21f8a250ca48b06db8cb922b4e3d
pivnati <natanmuleta911@gmail.com>
2024-08-12 22:16:35 -0700

    adding the  searchfild from the extract method

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,57 +9,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0xff1D1617).withOpacity(0.11),
-                  blurRadius: 40,
-                  spreadRadius: 0.0,
-                  offset: const Offset(0, 4),
-                ),
-              ],
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.all(15),
-                hintText: 'search for pancakes',
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: SvgPicture.asset('assets/icons/search.svg'),
-                ),
-                suffixIcon:Container(
-                  width: 100,
-                  child: IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        const VerticalDivider(
-                          color:Colors.black,
-                          indent: 10,
-                          endIndent: 10,
-                          thickness: 0.4,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(12),
-                          child: SvgPicture.asset('assets/icons/filter.svg'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ) ,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-          ),
+          _searchField(),
         ],
       ),
       appBar: AppBar(
@@ -101,5 +54,57 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.all(15),
+              hintText: 'search for pancakes',
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/search.svg'),
+              ),
+              suffixIcon:Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color:Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.4,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset('assets/icons/filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+              ) ,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        );
   }
 }
